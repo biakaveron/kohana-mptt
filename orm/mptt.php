23d013c5188c0d3b569bee39e083a4d58733c888
biakaveron <biakaveron@gmail.com>
2009-09-10 22:37:39 +0400

    new ORM logic applied

--- orm/mptt.php
+++ orm/mptt.php
@@ -7,22 +7,21 @@
  * @author     Brotkin Ivan (BIakaVeron) <BIakaVeron@gmail.com>
  * @copyright  Copyright (c) 2009 Brotkin Ivan
  *
- * @property Database												$db
- * @property Database_Query_Builder_Select  $db_builder
+ * @property Database                       $_db												$db
+ * @property Database_Query_Builder_Select  $_db_builder
  */
 
 abstract class ORM_MPTT extends ORM
 {
-	protected $left_column = 'lft';
-	protected $right_column = 'rgt';
-	protected $level_column = 'lvl';
-	protected $scope_column = 'scope';
-	protected $parent_column = 'parent_id';
-	protected $sorting;
+	protected $_left_column = 'lft';
+	protected $_right_column = 'rgt';
+	protected $_level_column = 'lvl';
+	protected $_scope_column = 'scope';
+	protected $_parent_column = 'parent_id';
 
 	public function __construct($id = NULL) {
-		if (!isset($this->sorting)) {
-			$this->sorting = array($this->left_column => 'ASC');
+		if (!isset($this->_sorting)) {
+			$this->_sorting = array($this->_left_column => 'ASC');
 		}
 		parent::__construct($id);
 	}
@@ -34,7 +33,7 @@ abstract class ORM_MPTT extends ORM
 
 	public function save() {
 		// overload basic ORM::save() method
-		if ( !$this->loaded) {
+		if ( !$this->loaded()) {
 			return $this->make_root();
 		}
 		else {
@@ -44,18 +43,18 @@ abstract class ORM_MPTT extends ORM
 
 	public function make_root($scope = NULL) {
 		// save node as root
-		if ($this->loaded) throw new Kohana_Exception('Cannot insert the same node twice');
+		if ($this->loaded()) throw new Kohana_Exception('Cannot insert the same node twice');
 		if (is_null($scope)) {
 			$scope = self::get_next_scope();
 		}
 		elseif (self::scope_available($scope) === FALSE) {
 			$scope = self::get_next_scope();
 		}
-		$this->{$this->scope_column} = $scope;
-		$this->{$this->level_column} = 1;
-		$this->{$this->left_column} = 1;
-		$this->{$this->right_column} = 2;
-		$this->{$this->parent_column} = NULL;
+		$this->{$this->_scope_column} = $scope;
+		$this->{$this->_level_column} = 1;
+		$this->{$this->_left_column} = 1;
+		$this->{$this->_right_column} = 2;
+		$this->{$this->_parent_column} = NULL;
 		return parent::save();
 	}
 
@@ -63,20 +62,20 @@ abstract class ORM_MPTT extends ORM
 		// inserts node as direct child for $id node
 		$this->lock();
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		if ($first === TRUE) {
-			$lft = $id->{$this->left_column}+1;
+			$lft = $id->{$this->_left_column}+1;
 		}
 		else {
-			$lft = $id->{$this->right_column};
+			$lft = $id->{$this->_right_column};
 		}
-		$this->{$this->scope_column} = $id->scope();
+		$this->{$this->_scope_column} = $id->scope();
 		$this->add_space($lft, 2);
-		$this->{$this->parent_column} = $id->primary_key_value;
-		$this->{$this->level_column} = $id->level() + 1;
-		$this->{$this->left_column} = $lft;
-		$this->{$this->right_column} = $lft+1;
+		$this->{$this->_parent_column} = $id->primary_key_value;
+		$this->{$this->_level_column} = $id->level() + 1;
+		$this->{$this->_left_column} = $lft;
+		$this->{$this->_right_column} = $lft+1;
 		parent::save();
 		$this->unlock();
 		return $this;
@@ -87,7 +86,7 @@ abstract class ORM_MPTT extends ORM
 		if ($this->loaded) throw new Kohana_Exception('Cannot insert the same node twice');
 		if ($this->size() > 2) throw new Kohana_Exception('Cannot use a node with children');
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		if ($before) {
 			$lft = $id->left();
@@ -95,13 +94,13 @@ abstract class ORM_MPTT extends ORM
 		else {
 			$lft = $id->right() + 1;
 		}
-		$this->{$this->scope_column} = $id->scope();
+		$this->{$this->_scope_column} = $id->scope();
 		$this->lock();
 		$this->add_space($lft);
-		$this->{$this->left_column} = $lft;
-		$this->{$this->right_column} = $lft+1;
-		$this->{$this->parent_column} = $id->parent();
-		$this->{$this->level_column} = $id->level();
+		$this->{$this->_left_column} = $lft;
+		$this->{$this->_right_column} = $lft+1;
+		$this->{$this->_parent_column} = $id->parent();
+		$this->{$this->_level_column} = $id->level();
 		parent::save();
 		$this->unlock();
 	}
@@ -109,10 +108,10 @@ abstract class ORM_MPTT extends ORM
 	public function delete() {
 		// deletes current node with descendants
 		$this->lock();
-		DB::delete($this->table_name)
-			->where($this->left_column," >=",$this->left())
-			->where($this->left_column," <= ",$this->right())
-			->execute($this->db);
+		DB::delete($this->_table_name)
+			->where($this->_left_column," >=",$this->left())
+			->where($this->_left_column," <= ",$this->right())
+			->execute($this->_db);
 		$this->clear_space($this->left(), $this->size());
 		$this->unlock();
 	}
@@ -120,7 +119,7 @@ abstract class ORM_MPTT extends ORM
 	public function move_to($id, $first = FALSE) {
 		// moves current node with descendants to a node $id
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		if ($this->is_in_descendants($id)) {
 			throw new Kohana_Exception('Cannot move nodes to themself');
@@ -135,24 +134,24 @@ abstract class ORM_MPTT extends ORM
 		if ($deltalevel < 0) $deltalevel = "(".$deltalevel.")";
 		$this->lock();
 		// temporary setting scope to 0
-		DB::update($this->table_name)
+		DB::update($this->_table_name)
 			->in($this->primary_key, $ids)
-			->set(array($this->scope_column => 0))
-			->execute($this->db);
+			->set(array($this->_scope_column => 0))
+			->execute($this->_db);
 		$this->clear_space($oldlft, $this->size());
-		$this->{$this->scope_column} = $id->scope();
+		$this->{$this->_scope_column} = $id->scope();
 		$this->add_space($lft, $this->size());
 
-		DB::update($this->table_name)
+		DB::update($this->_table_name)
 			->in($this->primary_key, $ids)
 			->set(array(
-					$this->left_column => DB::expr($this->left_column. " + ".$delta),
-					$this->right_column => DB::expr($this->right_column. " + ".$delta),
-					$this->level_column => DB::expr($this->level_column. " + ".$deltalevel),
-					$this->scope_column => $id->scope(),		
+				$this->_left_column => DB::expr($this->_left_column. " + ".$delta),
+				$this->_right_column => DB::expr($this->_right_column. " + ".$delta),
+				$this->_level_column => DB::expr($this->_level_column. " + ".$deltalevel),
+				$this->_scope_column => $id->scope(),
 			))
-			->execute($this->db);
-		$this->{$this->parent_column} = $id->primary_key_value;
+			->execute($this->_db);
+		$this->{$this->_parent_column} = $id->primary_key_value;
 		parent::save();
 		$this->unlock();
 	}
@@ -161,7 +160,7 @@ abstract class ORM_MPTT extends ORM
 		// moves all descendants to $id node WITHOUT current node
 		if (!$this->has_children()) return FALSE;
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		$ids = $this->subtree(FALSE)->primary_key_array();
 		$lft = ($first==TRUE ? $id->left() + 1 : $id->right());
@@ -172,29 +171,29 @@ abstract class ORM_MPTT extends ORM
 		$deltalevel = $level - $this->level() - 1;
 		if ($deltalevel < 0) $deltalevel = "(".$deltalevel.")";
 		$this->lock();
-		DB::update($this->table_name)
+		DB::update($this->_table_name)
 			->in($this->primary_key, $ids)
-			->set(array($this->scope_column => 0))
-			->execute($this->db);
+			->set(array($this->_scope_column => 0))
+			->execute($this->_db);
 		$this->clear_space($oldlft, $this->size() - 2);
 		// this is need for correct add_space() work
-		$this->{$this->scope_column} = $id->scope();
+		$this->{$this->_scope_column} = $id->scope();
 		$this->add_space($lft, $this->size() - 2);
 
-		DB::update($this->table_name)
+		DB::update($this->_table_name)
 			->in($this->primary_key, $ids)
 			->set(array(
-					$this->left_column => DB::expr($this->left_column. " + ".$delta),
-					$this->right_column => DB::expr($this->right_column. " + ".$delta),
-					$this->level_column => DB::expr($this->level_column. " + ".$deltalevel),
-					$this->scope_column => $id->scope(),
-				))
-			->execute($this->db);
-		DB::update($this->table_name)
-			->set(array($this->parent_column => $id->primary_key_value))
-			->where($this->level_column, "=", $id->level() + 1)
+				$this->_left_column => DB::expr($this->_left_column. " + ".$delta),
+				$this->_right_column => DB::expr($this->_right_column. " + ".$delta),
+				$this->_level_column => DB::expr($this->_level_column. " + ".$deltalevel),
+				$this->_scope_column => $id->scope(),
+			))
+			->execute($this->_db);
+		DB::update($this->_table_name)
+			->set(array($this->_parent_column => $id->primary_key_value))
+			->where($this->_level_column, "=", $id->level() + 1)
 			->in($this->primary_key, $ids)
-			->execute($this->db);
+			->execute($this->_db);
 		$this->unlock();
 		$this->reload();
 	}
@@ -207,15 +206,15 @@ abstract class ORM_MPTT extends ORM
 	public function get_root($scope = NULL) {
 		if (is_null($scope)) {
 			// returns all roots
-			return self::factory($this->object_name)
-				->where($this->left_column, "=", 1)
+			return self::factory($this->_object_name)
+				->where($this->_left_column, "=", 1)
 				->find_all();
 		}
 		else {
 			// only current root
-			return self::factory($this->object_name)
-				->where($this->scope_column, "=", $scope)
-				->andwhere($this->left_column, "=", 1)
+			return self::factory($this->_object_name)
+				->where($this->_scope_column, "=", $scope)
+				->andwhere($this->_left_column, "=", 1)
 				->find();
 		}
 	}
@@ -223,56 +222,56 @@ abstract class ORM_MPTT extends ORM
 	public function get_parents($with_self = FALSE) {
 		$suffix = $with_self ? "= " : " ";
 		// returns all current node parents
-		return self::factory($this->object_name)
-			->where($this->left_column," <",$suffix.$this->left())
-			->where($this->right_column," >",$suffix.$this->right())
-			->where($this->scope_column, "=", $this->scope())
+		return self::factory($this->_object_name)
+			->where($this->_left_column," <",$suffix.$this->left())
+			->where($this->_right_column," >",$suffix.$this->right())
+			->where($this->_scope_column, "=", $this->scope())
 			->find_all();
 	}
 
 	public function get_parent() {
 		if ($this->is_root()) return NULL;
-		return self::factory($this->object_name, $this->parent());
+		return self::factory($this->_object_name, $this->parent());
 	}
 
 	public function get_children() {
 		// returns only direct children
-		return self::factory($this->object_name)
-			->where($this->left_column," >",$this->left())
-			->where($this->right_column," <",$this->right())
-			->where($this->scope_column, "=", $this->scope())
-			->where($this->level_column, "=", $this->level() + 1)
+		return self::factory($this->_object_name)
+			->where($this->_left_column," >",$this->left())
+			->where($this->_right_column," <",$this->right())
+			->where($this->_scope_column, "=", $this->scope())
+			->where($this->_level_column, "=", $this->level() + 1)
 			->find_all();
 	}
 
 	public function get_subtree($with_parent = FALSE) {
 		// return all descendants of current node
 		$suffix = ($with_parent ? "= " : " ");
-		return self::factory($this->object_name)
-			->where($this->left_column," >",$suffix.$this->left())
-			->where($this->right_column," <",$suffix.$this->right())
-			->where($this->scope_column, "=", $this->scope())
+		return self::factory($this->_object_name)
+			->where($this->_left_column," >",$suffix.$this->left())
+			->where($this->_right_column," <",$suffix.$this->right())
+			->where($this->_scope_column, "=", $this->scope())
 			->find_all();
 	}
 	
 	public function get_fulltree($use_scope = TRUE) {
 		// returns full tree (with or without scope checking)
-		$result = self::factory($this->object_name);
+		$result = self::factory($this->_object_name);
 		if ($use_scope) 
-			$result->where($this->scope_column, "=", $this->{$this->scope_column});
+			$result->where($this->_scope_column, "=", $this->{$this->_scope_column});
 		if ($use_scope == FALSE) 
-			$result->orderby($this->scope_column, 'ASC')
-				->orderby($this->left_column, 'ASC');
+			$result->orderby($this->_scope_column, 'ASC')
+				->orderby($this->_left_column, 'ASC');
 		return $result->find_all();
 	}
 
 	public function get_leaves() {
 		// returns only leaves of current node
-		return self::factory($this->object_name)
-			->where($this->left_column," >",$suffix.$this->left())
-			->where($this->right_column," <",$suffix.$this->right())
-			->where($this->left_column, "=", DB::expr($this->right_column." - 1"))
-			->where($this->scope_column, "=", $this->scope())
+		return self::factory($this->_object_name)
+			->where($this->_left_column," >",$suffix.$this->left())
+			->where($this->_right_column," <",$suffix.$this->right())
+			->where($this->_left_column, "=", DB::expr($this->_right_column." - 1"))
+			->where($this->_scope_column, "=", $this->scope())
 			->find_all();
 	}
 
@@ -287,27 +286,27 @@ abstract class ORM_MPTT extends ORM
 	}
 
  	public function left() {
-		return $this->{$this->left_column};
+		return $this->{$this->_left_column};
 	}
 
 	public function right() {
-		return $this->{$this->right_column};
+		return $this->{$this->_right_column};
 	}
 
 	public function level() {
-		return $this->{$this->level_column};
+		return $this->{$this->_level_column};
 	}
 
 	public function scope() {
-		return $this->{$this->scope_column};
+		return $this->{$this->_scope_column};
 	}
 
 	public function parent() {
-		return $this->{$this->parent_column};
+		return $this->{$this->_parent_column};
 	}
 
 	public function size() {
-		return $this->{$this->right_column} - $this->left() + 1;
+		return $this->{$this->_right_column} - $this->left() + 1;
 	}
 
 	public function count() {
@@ -321,23 +320,23 @@ abstract class ORM_MPTT extends ORM
 	public function is_parent($id) {
 		// is current node a direct parent of $id node
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
-		return $id->{$this->parent_column} == $this->primary_key_value;
+		return $id->{$this->_parent_column} == $this->primary_key_value;
 	}
 
 	public function is_child($id) {
 		// is current node a direct child of $id node
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
-		return $this->{$this->parent_column} == $id->primary_key_value;
+		return $this->{$this->_parent_column} == $id->primary_key_value;
 	}
 
 	public function is_in_descendants($id) {
 		// is current node one of a $id node child
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		if ($this->scope() != $id->scope()) return FALSE;
 		if ($this->left() <= $id->left()) return FALSE;
@@ -348,7 +347,7 @@ abstract class ORM_MPTT extends ORM
 	public function is_in_parents($id) {
 		// is current node one of a $id node parents
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		return $id->is_in_descendants($this);
 	}
@@ -356,7 +355,7 @@ abstract class ORM_MPTT extends ORM
 	public function is_neighbor($id) {
 		// is current node neighbor of $id node (the same direct parent)
 		if (!is_a($id, get_class($this))) {
-			$id = self::factory($this->object_name, $id);
+			$id = self::factory($this->_object_name, $id);
 		}
 		return ($this->parent() == $id->parent());
 	}
@@ -374,55 +373,55 @@ abstract class ORM_MPTT extends ORM
 	protected function add_space($start, $size = 2) {
 		// add space for adding/inserting nodes
 		// $this->scope should be set before adding space!
-		DB::update($this->table_name)
-			->set(array($this->left_column => DB::expr($this->left_column.' + '.$size)))
-			->where($this->left_column," >= ",$start)
-			->where($this->scope_column, "=", $this->scope())
-			->execute($this->db);
-		DB::update($this->table_name)
-			->set(array($this->right_column => DB::expr($this->right_column.' + '.$size)))
-			->where($this->right_column," >= ",$start)
-			->where($this->scope_column, "=", $this->scope())
-			->execute($this->db);
+		DB::update($this->_table_name)
+			->set(array($this->_left_column => DB::expr($this->_left_column.' + '.$size)))
+			->where($this->_left_column," >= ",$start)
+			->where($this->_scope_column, "=", $this->scope())
+			->execute($this->_db);
+		DB::update($this->_table_name)
+			->set(array($this->_right_column => DB::expr($this->_right_column.' + '.$size)))
+			->where($this->_right_column," >= ",$start)
+			->where($this->_scope_column, "=", $this->scope())
+			->execute($this->_db);
 	}
 
 	protected function clear_space($start, $size = 2) {
 		// remove space after deleting/moving node
-		DB::update($this->table_name)
-			->set(array($this->left_column => DB::expr($this->left_column.' - '.$size)))
-			->where($this->left_column," >= ",$start)
-			->where($this->scope_column, "=", $this->scope())
-			->execute($this->db);
-		DB::update($this->table_name)
-			->set(array($this->right_column => DB::expr($this->right_column.' - '.$size)))
-			->where($this->right_column," >= ",$start)
-			->where($this->scope_column, "=", $this->scope())
-			->execute($this->db);
+		DB::update($this->_table_name)
+			->set(array($this->_left_column => DB::expr($this->_left_column.' - '.$size)))
+			->where($this->_left_column," >= ",$start)
+			->where($this->_scope_column, "=", $this->scope())
+			->execute($this->_db);
+		DB::update($this->_table_name)
+			->set(array($this->_right_column => DB::expr($this->_right_column.' - '.$size)))
+			->where($this->_right_column," >= ",$start)
+			->where($this->_scope_column, "=", $this->scope())
+			->execute($this->_db);
 	}
 
 	protected function lock() {
 		// lock table
-		$this->db->query('lock','LOCK TABLE '.$this->table_name.' WRITE');
+		$this->_db->query('lock','LOCK TABLE '.$this->_table_name.' WRITE');
 	}
 
 	protected function unlock() {
 		// unlock tables
-		$this->db->query('unlock','UNLOCK TABLES');
+		$this->_db->query('unlock','UNLOCK TABLES');
 	}
 
 	protected function scope_available($scope) {
 		// checking for supplied scope available
-		return ! self::factory($this->object_name)
-			->where($this->scope_column, "=", $scope)
+		return ! self::factory($this->_object_name)
+			->where($this->_scope_column, "=", $scope)
 			->count_all();
 	}
 
 	protected function get_next_scope() {
 		// returns available value for scope
-		$scope = DB::select(DB::expr('IFNULL(MAX(`'.$this->scope_column.'`), 0) as scope'))
-					->from($this->table_name)
-					->execute($this->db)
-					->current();
+		$scope = DB::select(DB::expr('IFNULL(MAX(`'.$this->_scope_column.'`), 0) as scope'))
+				->from($this->_table_name)
+				->execute($this->_db)
+				->current();
 		if ($scope AND intval($scope['scope'])>0) return intval($scope['scope'])+1;
 		return 1;
 	}
